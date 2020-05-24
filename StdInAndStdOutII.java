08d2e6769a37df7f4b0d62d84944dcdd2c68dd8b
SGrannemann <64766572+SGrannemann@users.noreply.github.com>
2020-05-24 08:59:21 +0200

    Added Solution to first If-Else and second Standard in- and output challenge.

---
+++ StdInAndStdOutII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       // int i = scan.nextInt();
+        int i = Integer.valueOf(scan.nextLine());
+        double d = Double.valueOf(scan.nextLine());
+        String s = scan.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
