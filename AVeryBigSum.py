d1a87fd77fcf06fcb68f6376236b3b8599f7ee21
Soeren Grannemann <64766572+SGrannemann@users.noreply.github.com>
2021-01-02 12:08:27 +0100

    Solved 'A very big sum' and 'compare triplets'.

---
+++ AVeryBigSum.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+"""
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+int ar[n]: an array of integers .
+Return
+
+long: the sum of all array elements
+Input Format
+
+The first line of the input consists of an integer .
+The next line contains  space-separated integers contained in the array.
+
+Output Format
+
+Return the integer sum of the elements in the array.
+"""
+import math
+import os
+import random
+import re
+import sys
+from functools import reduce
+
+
+
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    return sum(ar)
+    #return reduce(lambda x, y: x+y, ar)
+
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
